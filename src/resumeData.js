666dc84915695bca8c0d2cd87fed51880d9c3689
saranyachandrukumar <tamil.saran0@gmail.com>
2023-08-31 09:35:22 +0400

    first commit

---
+++ src/resumeData.js
@@ -0,0 +1,116 @@
+let resumeData = {
+    "imagebaseurl":"https://rbhatia46.github.io/",
+    "name": "Saranya",
+    "role": "React Developer ",
+    "linkedinId":"Your LinkedIn Id",
+    "skypeid": "Your skypeid",
+    "roleDescription": "I like dabbling in various parts of frontend development and like to learn about new technologies.",
+    "socialLinks":[
+        // {
+        //   "name":"linkedin",
+        //   "url":"https://www.linkedin.com/in/rahul-bhatia-67ba08121/",
+        //   "className":"fa fa-linkedin"
+        // },
+        {
+          "name":"github",
+          "url":"https://github.com/saranyachandrukumar",
+          "className":"fa fa-github"
+        },
+        // {
+        //   "name":"skype",
+        //   "url":"http://twitter.com/rbhatia46",
+        //   "className":"fa fa-twitter"
+        // }
+      ],
+    "aboutme":"I Started my carrier in Non-IT and my interest in coding I learned frontend development. I am a self taught React Developer, I believe that to be successful in life, one needs to be obsessive with their dreams and keep working towards them.",
+    "address":"UAE",
+    "website":"https://github.com/saranyachandrukumar",
+    "education":[
+      {
+        "UniversityName":"Bharathiyar university",
+        "specialization":"MSC-IT",
+        // "MonthOfPassing":"Aug",
+        "YearOfPassing":"2013",
+      },
+      {
+        "UniversityName":"Park's College",
+        "specialization":"BSC-IT",
+        // "MonthOfPassing":"JUN",
+        "YearOfPassing":"2011",
+        "Achievements":"First class with Distinction"
+      }
+    ],
+    "work":[
+      {
+        "CompanyName":"KGIT",
+        "specialization":"Support Engineer",
+        // "MonthOfLeaving":"J",
+        "YearOfLeaving":"Till Date",
+        // "Achievements":"Some Achievements"
+      },
+      {
+        "CompanyName":"Pathfinder Business analysis private limited",
+        "specialization":"Data analyst",
+        "MonthOfLeaving":"May",
+        "YearOfLeaving":"2015",
+        // "Achievements":"I got best performer award"
+      }
+    ],
+    "skillsDescription":"Your skills here",
+    "skills":[
+      {
+        "skillname":"HTML"
+      },
+      
+      {
+        "skillname":"CSS"
+      },
+      {
+        "skillname":"Javascript"
+      },
+      {
+        "skillname":"ReactJs"
+      },
+      {
+        "skillname":"Git"
+      },
+      {
+        "skillname":"Python and Django"
+      },
+
+    ],
+    "portfolio":[
+      {
+        "name":"project1",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/login.jpeg"
+      },
+      {
+        "name":"project2",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/volunteer.JPG"
+      },
+      {
+        "name":"project3",
+        "description":"mobileapp",  
+        "imgurl":"images/portfolio/contact form.JPG"
+      },
+      {
+        "name":"project4",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/flourwebsite.jpg"
+      }
+    ],
+    // "testimonials":[
+    //   {
+    //     "description":"This is a sample testimonial",
+    //     "name":"Some technical guy"
+    //   },
+    //   {
+    //     "description":"This is a sample testimonial",
+    //     "name":"Some technical guy"
+    //   }
+    // ]
+  }
+  
+  export default resumeData
